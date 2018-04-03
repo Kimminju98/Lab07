51ce54a3ecdf7c727e5f8d6c33bd3d36c6505cd1
Kimminju98 <21700097@handong.edu>
2018-04-03 23:50:04 +0900

    Finish my code

--- src/main/java/edu/handong/cess/java/lab07/prob3/Attendance.java
+++ src/main/java/edu/handong/cess/java/lab07/prob3/Attendance.java
@@ -1,5 +1,56 @@
 package edu.handong.cess.java.lab07.prob3;
 
 public class Attendance {
+	
+	private String name;
+	private int grade;
+	private int id;
+	private int absent;
+	
 
+	public Attendance(String name,int grade,int id) {
+		this.name=name;
+		this.grade=grade;
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getGrade() {
+		return grade;
+	}
+
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getAbsent() {
+		return absent;
+	}
+
+
+	public void setAbsent(int absent) {
+		this.absent = absent;
+	}
 }
--- src/main/java/edu/handong/cess/java/lab07/prob3/Attendence_main.java
+++ src/main/java/edu/handong/cess/java/lab07/prob3/Attendence_main.java
@@ -1,5 +1,47 @@
 package edu.handong.cess.java.lab07.prob3;
+import java.util.Random;
 
 public class Attendence_main {
 
+	public static void main(String [] args) {
+		
+		Random randomGen = new Random();
+		int absent;
+		
+		Attendance s1 = new Attendance("Jess",4,21400999);
+		Attendance s2 = new Attendance("Kent",2,21700111);
+		Attendance s3 = new Attendance("Lucas",1,21833222);
+		Attendance s4 = new Attendance("Martha",4,21733444);
+		
+		absent = randomGen.nextInt(10);
+		s1.setAbsent(absent);
+		
+		absent = randomGen.nextInt(10);
+		s2.setAbsent(absent);
+		
+		absent = randomGen.nextInt(10);
+		s3.setAbsent(absent);
+		
+		absent = randomGen.nextInt(10);
+		s4.setAbsent(absent);
+		
+		if(s1.getAbsent() > 6) {
+			System.out.println("I'm sorry "+s1.getName()+" You fail this course");
+		}
+		else System.out.println("We'll see about the grade, "+s1.getName());
+		if(s2.getAbsent() > 6) {
+			System.out.println("I'm sorry "+s2.getName()+" You fail this course");
+		}
+		else System.out.println("We'll see about the grade, "+s2.getName());
+		if(s3.getAbsent() > 6) {
+			System.out.println("I'm sorry "+s3.getName()+" You fail this course");
+		}
+		else System.out.println("We'll see about the grade, "+s3.getName());
+		if(s4.getAbsent() > 6) {
+			System.out.println("I'm sorry "+s4.getName()+" You fail this course");
+		}
+		else System.out.println("We'll see about the grade, "+s4.getName());
+		
+	}
+	
 }
