93d661192fc63379ce9391a8160bb17500124d03
Kimminju98 <21700097@handong.edu>
2018-04-03 23:07:42 +0900

    Finish my Chicken source code

--- src/main/java/edu/handong/cess/java/lab07/prob2/Chicken.java
+++ src/main/java/edu/handong/cess/java/lab07/prob2/Chicken.java
@@ -1,5 +1,35 @@
 package edu.handong.cess.java.lab07.prob2;
 
 public class Chicken {
-
+	
+	private String name;
+	private int price;
+	private int stars;
+	
+	
+	public Chicken(String name,int price, int stars) {
+		this.name=name;
+		this.price=price;
+		this.stars=stars;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStars() {
+		return stars;
+	}
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+	
 }
--- src/main/java/edu/handong/cess/java/lab07/prob2/Chicken_main.java
+++ src/main/java/edu/handong/cess/java/lab07/prob2/Chicken_main.java
@@ -2,4 +2,16 @@ package edu.handong.cess.java.lab07.prob2;
 
 public class Chicken_main {
 
+	public static void main(String []args) {
+		
+		Chicken m1 = new Chicken("Cheese_mustard",16000,3);
+		Chicken m2 = new Chicken("Honey_mustard",16000,4);
+		Chicken m3 = new Chicken("Spicey_chicken",15000,1);
+		
+		System.out.println("menu1 : "+m1.getName()+"'s rating is "+m1.getStars());
+		System.out.println("menu1 : "+m2.getName()+"'s rating is "+m2.getStars());
+		System.out.println("menu1 : "+m3.getName()+"'s rating is "+m3.getStars());
+	}
+	
+	
 }
