2fbca2eb5ab997a39db2c0ae8583b1859d338e44
Kimminju98 <21700097@handong.edu>
2018-04-04 13:43:17 +0900

    Add java doc

--- src/main/java/edu/handong/cess/java/lab07/prob2/Chicken.java
+++ src/main/java/edu/handong/cess/java/lab07/prob2/Chicken.java
@@ -1,35 +1,60 @@
 package edu.handong.cess.java.lab07.prob2;
-
+/**
+ * This is Chicken class
+ * This class has Chicken(),getName(),setName(),getPrice(),setPrice(),getStars(),setStars()method.
+ * This class has name,price,stars members
+ * 
+ * @author User
+ *
+ */
 public class Chicken {
 	
-	private String name;
-	private int price;
-	private int stars;
+	private String name; //This is Chicken class's member, name
+	private int price;//This is Chicken class's member, price
+	private int stars;//This is Chicken class's member, stars
 	
-	
+	/**
+	 * This is Chicken class, constructor
+	 * This method put parameter to private variable.
+	 * @param name
+	 * @param price
+	 * @param stars
+	 */
 	public Chicken(String name,int price, int stars) {
-		this.name=name;
-		this.price=price;
-		this.stars=stars;
+		this.name=name; // Put parameter name to private name
+		this.price=price; // Put parameter price to private price
+		this.stars=stars; // Put parameter stars to private stars
 	}
-	
+	/**
+	 * This is getName method.
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
-		this.name = name;
+		this.name = name; // Put parameter name to private name
 	}
+	/**
+	 * This is getPrice method.
+	 * @return price
+	 */
 	public int getPrice() {
 		return price;
 	}
 	public void setPrice(int price) {
-		this.price = price;
+		this.price = price; // Put parameter price to private price
 	}
+	/**
+	 * This is getStars method.
+	 * @return stars
+	 */
 	public int getStars() {
 		return stars;
 	}
 	public void setStars(int stars) {
-		this.stars = stars;
+		this.stars = stars; // Put parameter stars to private stars
 	}
 	
 }
--- src/main/java/edu/handong/cess/java/lab07/prob2/Chicken_main.java
+++ src/main/java/edu/handong/cess/java/lab07/prob2/Chicken_main.java
@@ -1,16 +1,32 @@
 package edu.handong.cess.java.lab07.prob2;
 
+/**
+ * This is Chicken_main class.
+ * This class has main() method.
+ * <p> This class show chicken menu (name + rating)
+ * 
+ * @author MinJu
+ *
+ */
+
 public class Chicken_main {
 
+	/**
+	 * This is implement main method.
+	 * This method call Chicken class and Chicken class's getName method and getStars method
+	 * and then print out m1, m2, m3 's chicken name and rating .
+	 * 
+ 	 * @param args
+	 */
 	public static void main(String []args) {
 		
-		Chicken m1 = new Chicken("Cheese_mustard",16000,3);
-		Chicken m2 = new Chicken("Honey_mustard",16000,4);
-		Chicken m3 = new Chicken("Spicey_chicken",15000,1);
+		Chicken m1 = new Chicken("Cheese_mustard",16000,3); // initiated Chicken class by using m1 and give name,price,rating to Chicken constructor
+		Chicken m2 = new Chicken("Honey_mustard",16000,4); // initiated Chicken class by using m2 and give name,price,rating to Chicken constructor
+		Chicken m3 = new Chicken("Spicey_chicken",15000,1); // initiated Chicken class by using m3 and give name,price,rating to Chicken constructor
 		
-		System.out.println("menu1 : "+m1.getName()+"'s rating is "+m1.getStars());
-		System.out.println("menu1 : "+m2.getName()+"'s rating is "+m2.getStars());
-		System.out.println("menu1 : "+m3.getName()+"'s rating is "+m3.getStars());
+		System.out.println("menu1 : "+m1.getName()+"'s rating is "+m1.getStars()); // print-out m1's chicken name and Stars
+		System.out.println("menu2 : "+m2.getName()+"'s rating is "+m2.getStars());// print-out m2's chicken name and Stars
+		System.out.println("menu3 : "+m3.getName()+"'s rating is "+m3.getStars());// print-out m3's chicken name and Stars
 	}
 	
 	
