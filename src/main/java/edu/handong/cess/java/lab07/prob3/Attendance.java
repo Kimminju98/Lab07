c02bb655b22e4cd3684acf564386121bdedf93ad
Kimminju98 <21700097@handong.edu>
2018-04-04 19:12:23 +0900

    Add java doc

--- src/main/java/edu/handong/cess/java/lab07/prob3/Attendance.java
+++ src/main/java/edu/handong/cess/java/lab07/prob3/Attendance.java
@@ -1,28 +1,50 @@
 package edu.handong.cess.java.lab07.prob3;
-
+/**
+ * This is Attendance class
+ * This class has Attendance(), getName(),setName(), getGrade(),setGrade(), getId(),setId(), getAbsent(),setAbsent() methods.
+ * This class has name,grade,id,absent members
+ * @author MinJu
+ *
+ */
 public class Attendance {
 	
-	private String name;
-	private int grade;
-	private int id;
-	private int absent;
+	private String name; // This is Attendance class's member, name
+	private int grade; // This is Attendance class's member, grade
+	private int id; // This is Attendance class's member, id
+	private int absent; // This is Attendance class's member, absent
 	
+	/**
+	 * This is Attendance method, constructor
+	 * This method put parameter to private variable.
+	 * 
+	 * @param name
+	 * @param grade
+	 * @param id
+	 */
 
 	public Attendance(String name,int grade,int id) {
-		this.name=name;
-		this.grade=grade;
-		this.id = id;
+		this.name=name; // put parameter name to private name
+		this.grade=grade; // put parameter grade to private grade
+		this.id = id; // put parameter id to private id
 	}
-
+ 
+	/**
+	 * This is getName method.
+	 * @return name (private)
+	 */
 
 	public String getName() {
-		return name;
+		return name; 
 	}
 
 
 	public void setName(String name) {
-		this.name = name;
+		this.name = name; // Put parameter name to private name
 	}
+	/**
+	 * This is getGrade method.
+	 * @return grade (private)
+	 */
 
 
 	public int getGrade() {
@@ -31,8 +53,12 @@ public class Attendance {
 
 
 	public void setGrade(int grade) {
-		this.grade = grade;
+		this.grade = grade; // Put parameter grade to private grade
 	}
+	/**
+	 * This is getId method.
+	 * @return id (private)
+	 */
 
 
 	public int getId() {
@@ -41,16 +67,25 @@ public class Attendance {
 
 
 	public void setId(int id) {
-		this.id = id;
+		this.id = id; // put parameter id to private id
 	}
 
+	/**
+	 * This is getAbsent method.
+	 * @return absent (private)
+	 */
 
 	public int getAbsent() {
 		return absent;
 	}
 
+	/**
+	 * This is setAbsent method.
+	 * This method put parameter absent to private absent
+	 * @param absent integer
+	 */
 
 	public void setAbsent(int absent) {
-		this.absent = absent;
+		this.absent = absent; // put parameter absent to private absent
 	}
 }
--- src/main/java/edu/handong/cess/java/lab07/prob3/Attendence_main.java
+++ src/main/java/edu/handong/cess/java/lab07/prob3/Attendence_main.java
@@ -1,46 +1,61 @@
 package edu.handong.cess.java.lab07.prob3;
 import java.util.Random;
-
+/**
+ * This is Attendence_main class
+ * It has main() method
+ * It has absent member
+ * <p> print out who is fail (if absent >6 , fail)
+ * @author User
+ *
+ */
 public class Attendence_main {
 
+	/**
+	 * This is implement, main method
+	 * This method make 4 Attendance instance and give name,grade,id parameter (s1,s2,s3,s4)
+	 * and then make random absent number by using Random class
+	 * This method call setAbsent and give random absent number to setAbsent
+	 * and call getAbsent Id and printout s1,2,3,4 name and fail or success message.
+	 * @param args
+	 */
 	public static void main(String [] args) {
 		
-		Random randomGen = new Random();
-		int absent;
+		Random randomGen = new Random(); // Initiate Random class, named randomGen for using Random class's method. 
+		int absent; // absent count variable
 		
-		Attendance s1 = new Attendance("Jess",4,21400999);
-		Attendance s2 = new Attendance("Kent",2,21700111);
-		Attendance s3 = new Attendance("Lucas",1,21833222);
-		Attendance s4 = new Attendance("Martha",4,21733444);
+		Attendance s1 = new Attendance("Jess",4,21400999); //Initiate Attendance class, named s1(student1)
+		Attendance s2 = new Attendance("Kent",2,21700111); //Initiate Attendance class, named s2(student2)
+		Attendance s3 = new Attendance("Lucas",1,21833222); //Initiate Attendance class, named s3(student3)
+		Attendance s4 = new Attendance("Martha",4,21733444); //Initiate Attendance class, named s4(student4)
 		
-		absent = randomGen.nextInt(10);
-		s1.setAbsent(absent);
+		absent = randomGen.nextInt(10); // put random number to absent (range : 0~10)
+		s1.setAbsent(absent); // Give absent value to s1(Attendance class)'s setAbsent method
 		
-		absent = randomGen.nextInt(10);
-		s2.setAbsent(absent);
+		absent = randomGen.nextInt(10);// put random number to absent (range : 0~10)
+		s2.setAbsent(absent); // Give absent value to s2(Attendance class)'s setAbsent method
 		
-		absent = randomGen.nextInt(10);
-		s3.setAbsent(absent);
+		absent = randomGen.nextInt(10);// put random number to absent (range : 0~10)
+		s3.setAbsent(absent); // Give absent value to s3(Attendance class)'s setAbsent method
 		
-		absent = randomGen.nextInt(10);
-		s4.setAbsent(absent);
+		absent = randomGen.nextInt(10);// put random number to absent (range : 0~10)
+		s4.setAbsent(absent); // Give absent value to s4(Attendance class)'s setAbsent method
 		
-		if(s1.getAbsent() > 6) {
-			System.out.println("I'm sorry "+s1.getName()+" You fail this course");
+		if(s1.getAbsent() > 6) { // if s1(Attendance class)'s absent value > 6
+			System.out.println("I'm sorry "+s1.getName()+" You fail this course"); // print-out s1's name and fail message
 		}
-		else System.out.println("We'll see about the grade, "+s1.getName());
-		if(s2.getAbsent() > 6) {
-			System.out.println("I'm sorry "+s2.getName()+" You fail this course");
+		else System.out.println("We'll see about the grade, "+s1.getName()); // if s1(Attendance class)'s absent value < 6, print-out s1's name and message
+		if(s2.getAbsent() > 6) {// if s2(Attendance class)'s absent value > 6
+			System.out.println("I'm sorry "+s2.getName()+" You fail this course"); // print-out s2's name and fail message
 		}
-		else System.out.println("We'll see about the grade, "+s2.getName());
-		if(s3.getAbsent() > 6) {
-			System.out.println("I'm sorry "+s3.getName()+" You fail this course");
+		else System.out.println("We'll see about the grade, "+s2.getName());// if s2(Attendance class)'s absent value < 6, print-out s2's name and message
+		if(s3.getAbsent() > 6) { // if s3(Attendance class)'s absent value > 6
+			System.out.println("I'm sorry "+s3.getName()+" You fail this course"); // print-out s3's name and fail message
 		}
-		else System.out.println("We'll see about the grade, "+s3.getName());
-		if(s4.getAbsent() > 6) {
-			System.out.println("I'm sorry "+s4.getName()+" You fail this course");
+		else System.out.println("We'll see about the grade, "+s3.getName());// if s3(Attendance class)'s absent value < 6, print-out s3's name and message
+		if(s4.getAbsent() > 6) { // if s4(Attendance class)'s absent value > 6
+			System.out.println("I'm sorry "+s4.getName()+" You fail this course"); // print-out s4's name and fail message
 		}
-		else System.out.println("We'll see about the grade, "+s4.getName());
+		else System.out.println("We'll see about the grade, "+s4.getName());// if s4(Attendance class)'s absent value < 6, print-out s4's name and message
 		
 	}
 	
